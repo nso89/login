c937f76eeb40185db80c5405fbc0276d75f4d8f1
nso89 <nsonline89@outlook.com>
2023-09-18 09:21:11 -0400

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+
+set keepass=%ProgramFiles%\KeePass Password Safe 2\KeePass.exe
+set browser=%ProgramFiles(x86)%\Mozilla Firefox\firefox.exe
+
+echo Welcome, %username%
+
+call:startup "%keepass%"
+call:startup "%browser%"
+
+exit /b
+
+:startup
+echo Starting %~1
+start "" "%~1"
+pause
+goto:eof 
